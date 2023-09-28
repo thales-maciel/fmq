ab8e3a6e1193a0694076b8a91933f587b6179262
Thales Maciel <thales@thalesmaciel.com>
2023-09-28 20:09:50 -0300

    implemented basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,135 @@
-use fmq::prelude::*;
+extern crate serde;
+extern crate serde_yaml;
+use std::{collections::HashMap, path::PathBuf, fs::read_to_string};
+use clap::Parser;
+use itertools::Itertools;
 
-use clap::{Parser, Subcommand};
+fn extract_front_matter(input: &str) -> Option<String> {
+    if !input.starts_with("---") {
+        return None;
+    }
 
-fn main() -> Result<()> {
-    Ok(())
+    let mut lines = input.lines();
+    lines.next();
+
+    let mut front_matter = String::new();
+    for line in lines {
+        if line == "---" {
+            return Some(front_matter);
+        }
+        front_matter.push_str(line);
+        front_matter.push('\n');
+    }
+    None
+}
+
+type Metadata = HashMap<String, serde_yaml::Value>;
+
+fn parse_front_matter(fm: &str) -> Result<Metadata, serde_yaml::Error> {
+    serde_yaml::from_str(fm)
+}
+
+struct Args {
+    pub select: Option<Vec<String>>,
+    pub condition: Option<String>,
+    pub sort_by: Option<String>,
+    pub paths: Vec<PathBuf>,
+}
+
+struct SourceFile {
+    path: PathBuf,
+    metadata: Metadata,
 }
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
 pub struct Cli {
-    #[command(subcommand)]
-    pub command: Commands,
+    pub files: Vec<PathBuf>,
+
+    #[arg(short, long)]
+    pub select: Option<String>,
+
+    #[arg(short, long)]
+    pub condition: Option<String>,
+
+    #[arg(short, long)]
+    pub order_by: Option<String>,
 }
 
-#[derive(Subcommand)]
-pub enum Commands {
+fn main() {
+    let cli = Cli::parse();
+
+    // validate paths are not empty
+    if cli.files.is_empty() { panic!("at least one file required") }
+    // validate every path is a file
+    for p in &cli.files { if !p.is_file() { panic!("path is not a file") } }
+
+    // Validate arguments:
+    let args = Args{
+        select: cli.select.map(|v| v.split(" ").map(|s| s.to_string()).collect()),
+        condition: cli.condition,
+        sort_by: cli.order_by,
+        paths: cli.files,
+    };
+
+    // Create a place to put the results into
+    let mut processed: Vec<SourceFile> = vec!();
+
+    // > Parallelize it. For each file:
+    for path in args.paths {
+        let file_contents = read_to_string(&path).unwrap();
+        let Some(Ok(fm)) = extract_front_matter(&file_contents)
+            .map(|s| parse_front_matter(&s)) else { continue; };
+
+        if let Some(cond) = &args.condition {
+            // TODO: parse the condition query
+            if fm.get(cond).is_none() { continue; }
+        }
+        processed.push(SourceFile { path, metadata: fm });
+    }
+
+    // > TODO: join back to the main thread
+
+    // > do we have to sort?
+    if let Some(sort_by) = args.sort_by {
+        processed
+            .sort_by(|a, b| {
+                let ay = a.metadata.get(&sort_by).unwrap_or(&serde_yaml::Value::Null);
+                let by = b.metadata.get(&sort_by).unwrap_or(&serde_yaml::Value::Null);
+                let lel = ay.partial_cmp(&by).unwrap_or(std::cmp::Ordering::Equal);
+                lel
+            })
+    };
+
+    if let Some(fields) = &args.select {
+        for res in processed {
+            let mut values: Vec<String> = vec!();
+            for field in fields {
+                let raw_value = res.metadata.get(field);
+                match raw_value {
+                    Some(raw) => {
+                        let mut val = serde_yaml::to_string(raw).unwrap_or("".into());
+                        val.pop();
+                        values.push(val);
+                    },
+                    None => values.push("null".into()),
+                }
+            }
+            let escaped_values: String = values.join(", ").escape_default().collect();
+            println!("{}, {}", res.path.display(), escaped_values);
+        }
+    } else {
+        for res in processed {
+            let mut values: Vec<String> = vec!();
+            for k in res.metadata.keys().sorted() {
+                let mut val = serde_yaml::to_string(res.metadata.get(k).unwrap()).unwrap_or("".into());
+                val.pop();
+                values.push(val);
+            }
+            let escaped_values: String = values.join(", ").escape_default().collect();
+            println!("{}, {}", res.path.display(), escaped_values);
+        }
+    }
 }
 
---
+++ tests/e2e_test.rs
@@ -0,0 +1,127 @@
+use std::path::PathBuf;
+
+use assert_cmd::Command;
+
+pub enum File {
+    Full,
+    Partial,
+    None,
+    Missing,
+    Invalid
+}
+
+fn path(file: File) -> PathBuf {
+    match file {
+        File::Full => "./tests/fixtures/full.md".try_into().unwrap(),
+        File::Partial => "./tests/fixtures/partial.md".try_into().unwrap(),
+        File::None => "./tests/fixtures/no-fm.md".try_into().unwrap(),
+        File::Missing => "./tests/fixtures/missing.md".try_into().unwrap(),
+        File::Invalid => "./tests/fixtures/invalid.md".try_into().unwrap(),
+    }
+}
+
+#[test]
+fn dry_run() {
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .assert();
+    assert.failure();
+}
+
+#[test]
+fn happy() {
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .arg(path(File::Full))
+        .assert();
+    assert
+        .success()
+        .stdout("./tests/fixtures/full.md, 2023-09-26, asdf jkl, 0, Lorem Ipsum\n");
+}
+
+#[test]
+fn select() {
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .args(&["-s", "title date missing"])
+        .arg(path(File::Full))
+        .assert();
+    assert
+        .success()
+        .stdout("./tests/fixtures/full.md, Lorem Ipsum, 2023-09-26, null\n");
+}
+
+#[test]
+fn sort() {
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .args(&["-s", "title date", "-o", "title"])
+        .arg(path(File::Full))
+        .arg(path(File::Partial))
+        .assert();
+    assert
+        .success()
+        .stdout("./tests/fixtures/partial.md, Another Title, 2023-09-27\n./tests/fixtures/full.md, Lorem Ipsum, 2023-09-26\n");
+}
+
+#[test]
+fn other_sort() {
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .args(&["-s", "title date", "-o", "date"])
+        .arg(path(File::Full))
+        .arg(path(File::Partial))
+        .assert();
+    assert
+        .success()
+        .stdout("./tests/fixtures/full.md, Lorem Ipsum, 2023-09-26\n./tests/fixtures/partial.md, Another Title, 2023-09-27\n");
+}
+
+#[test]
+fn condition() {
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .args(&["-s", "title date", "-c", "title"])
+        .arg(path(File::Full))
+        .arg(path(File::Partial))
+        .arg(path(File::Invalid))
+        .arg(path(File::None))
+        .assert();
+    assert
+        .success()
+        .stdout("./tests/fixtures/full.md, Lorem Ipsum, 2023-09-26\n./tests/fixtures/partial.md, Another Title, 2023-09-27\n");
+}
+
+#[test]
+fn no_args() {
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .arg(path(File::Full))
+        .assert();
+    assert.success();
+
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .arg(path(File::Partial))
+        .assert();
+    assert.success();
+
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .arg(path(File::Missing))
+        .assert();
+    assert.failure();
+
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .arg(path(File::None))
+        .assert();
+    assert.success();
+
+    let mut cmd = Command::cargo_bin("fmq").unwrap();
+    let assert = cmd
+        .arg(path(File::Invalid))
+        .assert();
+    assert.success();
+}
+
